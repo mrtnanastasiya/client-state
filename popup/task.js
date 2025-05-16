6c4197338ccc8c26c70a99ee60140bcd9fbd600c
mrtnanastasiya <145048739+mrtnanastasiya@users.noreply.github.com>
2025-05-16 14:15:35 +0300

    Код скорректирован

--- popup/task.js
+++ popup/task.js
@@ -1,8 +1,8 @@
 // Функция для установки cookie
 function setCookie(name, value, days) {
-    var expires = '';
+    const expires = '';
     if (days) {
-        var date = new Date();
+        const date = new Date();
         date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
         expires = '; expires=' + date.toUTCString();
     }
@@ -12,10 +12,10 @@ function setCookie(name, value, days) {
 
 // Функция для получения cookie по имени
 function getCookie(name) {
-    var nameEQ = name + '=';
-    var ca = document.cookie.split(';');
+    const nameEQ = name + '=';
+    const ca = document.cookie.split(';');
     for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
+        let c = ca[i];
         while (c.charAt(0) === ' ') c = c.substring(1, c.length);
         if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
     }
